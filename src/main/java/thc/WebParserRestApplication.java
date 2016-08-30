9e2b71ec3777282e5bb4107977e4e1e8f2d178a9
Timmy Wong <wongtim@clsa.com>
2016-08-30 15:40:51 +0800

    cleanup

--- src/main/java/thc/WebParserRestApplication.java
+++ src/main/java/thc/WebParserRestApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
