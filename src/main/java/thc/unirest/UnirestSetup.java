4c200b319f8b282d8be1b77788368f7d557190b3
Timmy Wong <thcathy@gmail.com>
2016-12-29 20:04:16 +0800

    fix ssl cert issue

--- src/main/java/thc/unirest/UnirestSetup.java
+++ src/main/java/thc/unirest/UnirestSetup.java
@@ -3,7 +3,13 @@ package thc.unirest;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.utils.Base64Coder;
 import org.apache.http.HttpHost;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.ssl.SSLContexts;
 import thc.util.ProxySetting;
+
+import javax.net.ssl.SSLContext;
 
 /**
  * Created by wongtim on 21/07/2016.
@@ -16,6 +22,26 @@ public class UnirestSetup {
         Unirest.setConcurrency(MAX_TOTAL_HTTP_CONNECTION, MAX_HTTP_CONNECTION_PER_ROUTE);
         setDefaultHeaders();
         setupProxy();
+        setupHttpClientTrustAllCert();
+    }
+
+    private static void setupHttpClientTrustAllCert() {
+        TrustStrategy acceptingTrustStrategy = (arg0, arg1) -> true;
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Unirest.setHttpClient(HttpClients.custom()
+                .setSSLContext(sslContext)
+                .build());
+
+        Unirest.setAsyncHttpClient(HttpAsyncClients.custom()
+                .setSSLContext(sslContext)
+                .build());
     }
 
     private static void setDefaultHeaders() {
