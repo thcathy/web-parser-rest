b9df3ca05c515f22aac909526591315474fd4019
Timmy Wong <thcathy@gmail.com>
2019-08-14 22:05:08 +0800

    use spring webflux

--- src/test/java/thc/controller/SearchControllerTest.java
+++ src/test/java/thc/controller/SearchControllerTest.java
@@ -4,7 +4,6 @@ import com.google.common.collect.Iterables;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import thc.domain.WebItem;
 import thc.parser.search.GoogleImageSearchRequest;
@@ -14,7 +13,8 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
 
 public class SearchControllerTest {
     @Mock
@@ -33,7 +33,7 @@ public class SearchControllerTest {
     @Test
     public void httpReturnIsCached() {
         var webItemList = List.of(new WebItem("url", "", 100, 100, ""));
-        when(parseService.process(any())).thenReturn(CompletableFuture.supplyAsync(() -> webItemList));
+        when(parseService.process(any())).thenReturn(CompletableFuture.supplyAsync(() -> webItemList), CompletableFuture.supplyAsync(() -> null));
         when(parseService.processFlux(any())).thenCallRealMethod();
 
         var result = (List<WebItem>) controller.searchImage("any").block();
@@ -43,7 +43,6 @@ public class SearchControllerTest {
         var result2 = (List<WebItem>) controller.searchImage("any").block();
         assertThat(result2.size()).isEqualTo(1);
         assertThat((result2.get(0)).url).isEqualTo("url");
-        Mockito.verify(parseService, times(1)).process(any());
     }
 
 }
