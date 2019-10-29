2df463a46cc291a886e5c06422d043fffd9832f8
Timmy Wong <thcathy@gmail.com>
2019-10-29 23:46:03 +0800

    fix: set user agent for async http client

--- src/main/java/thc/WebParserRestApplication.java
+++ src/main/java/thc/WebParserRestApplication.java
@@ -100,7 +100,7 @@ public class WebParserRestApplication {
 		} catch (SSLException e) {
 			e.printStackTrace();
 		}
-		return asyncHttpClient(config().setSslContext(sslContext));
+		return asyncHttpClient(config().setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36").setSslContext(sslContext));
 	}
 
 	/**
