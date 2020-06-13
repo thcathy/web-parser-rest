845ce3f02fa67745a64bfe1e27780e4876d7c9f4
Timmy Wong <thcathy@gmail.com>
2020-06-13 14:15:19 +0800

    feature: new realtime stock quote source from money18

--- src/test/java/thc/controller/FinanceControllerTest.java
+++ src/test/java/thc/controller/FinanceControllerTest.java
@@ -7,8 +7,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import reactor.core.publisher.Mono;
-import thc.parser.JsoupParseRequest;
 import thc.parser.finance.AastockStockQuoteRequest;
+import thc.parser.finance.Money18StockQuoteRequest;
 import thc.service.HttpParseService;
 import thc.service.JsoupParseService;
 
@@ -39,28 +39,31 @@ public class FinanceControllerTest {
     @Test
     public void hkQuotes_withSpecificSource_useSpecificParser() {
         when(jsoupParseService.process(any())).thenReturn(Mono.just(Optional.empty()));
-
         controller.hkQuotes("941,939,2800,2828,5,883", "aastocks").collectList().block();
         verify(jsoupParseService, times(6)).process(any(AastockStockQuoteRequest.class));
 
-        // TODO: replace yahoo quote
-        //controller.hkQuotes("941,939,2800,2828,5,883", "yahoo").collectList().block();
-        //verify(jsoupParseService, times(6)).process(any(YahooStockQuoteRequest.class));
+        when(httpParseService.processFlux(any())).thenReturn(Mono.just(Optional.empty()));
+        controller.hkQuotes("941,939,2800,2828,5,883", "money18").collectList().block();
+        verify(httpParseService, times(6)).processFlux(any(Money18StockQuoteRequest.class));
     }
 
     @Test
     public void hkQuotes_givenNullSource_shouldProcessCorrectly() {
         when(jsoupParseService.process(any())).thenReturn(Mono.just(Optional.empty()));
+        when(httpParseService.processFlux(any())).thenReturn(Mono.just(Optional.empty()));
 
         controller.hkQuotes("941,939,2800,2828,5,883", null).collectList().block();
-        verify(jsoupParseService, times(6)).process(any(JsoupParseRequest.class));
+        verify(jsoupParseService, atMost(6)).process(any());
+        verify(httpParseService, atMost(6)).process(any());
     }
 
     @Test
     public void hkQuotes_givenWrongSource_shouldProcessCorrectly() {
         when(jsoupParseService.process(any())).thenReturn(Mono.just(Optional.empty()));
+        when(httpParseService.processFlux(any())).thenReturn(Mono.just(Optional.empty()));
 
         controller.hkQuotes("941,939,2800,2828,5,883", "wrong parser").collectList().block();
-        verify(jsoupParseService, times(6)).process(any(JsoupParseRequest.class));
+        verify(jsoupParseService, atMost(6)).process(any());
+        verify(httpParseService, atMost(6)).process(any());
     }
 }
